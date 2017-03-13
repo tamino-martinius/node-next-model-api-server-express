5b58774b9899d29f5302fd62d9a27e8f4de47d66
tamino-martinius <dev@zaku.eu>
2017-03-13 18:51:57 +0100

    Refactored whole package

--- index.js
+++ index.js
@@ -4,116 +4,162 @@ const lodash = require('lodash');
 const express = require('express');
 const pluralize = require('pluralize');
 
-const compact = lodash.compact;
+const assign = lodash.assign;
+const defaults = lodash.defaults;
+const filter = lodash.filter;
 const isArray = lodash.isArray;
-const toLower = lodash.toLower;
+const omit = lodash.omit;
 
 module.exports = class NextModelApiServerExpress {
-  constructor(app, Klass) {
-    this.app = app;
-    this.Klass = Klass;
-    this.router = express.Router();
-
-    route('all', false, (req, res) => {
-      respond(res, this.scopedKlass(req).all);
-    });
-
-    route('first', false, (req, res) => {
-      respond(res, this.scopedKlass(req).first);
-    });
-
-    route('last', false, (req, res) => {
-      respond(res, this.scopedKlass(req).last);
-    });
-
-    route('count', false, (req, res) => {
-      respond(res, this.scopedKlass(req).count);
-    });
-
-    route('insert', false, (req, res) => {
-      respond(res, this.klass(req).save());
-    });
-
-    route('update', true, (req, res) => {
-      respond(res, this.klass(req).save());
-    });
-
-    route('delete', true, (req, res) => {
-      respond(res, this.klass(req).delete());
-    });
-
-    this.app.use(this.apiPath, this.router);
+  static responseHeaders(req, res, next) {
+    const headers = {
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Amz-Date, X-Api-Key',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,PATCH,PUT,DELETE',
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': req.get('Origin'),
+    };
+    if (req.method === 'OPTIONS') {
+      const maxAge = 2592000;
+      headers['Cache-Control'] = 'public, max-age=' + maxAge;
+      headers['Expires'] = new Date(Date.now() + maxAge * 1000).toUTCString();
+      res.set(headers);
+      res.sendStatus(200);
+    } else {
+      res.set(headers);
+      return next();
+    }
   }
 
-  respond(res, promise) {
+  static respond(res, promise) {
     promise
       .then(data => this.handleData(res, data))
       .catch(err => this.handleError(res, err))
     ;
   }
 
-  handleData(res, data) {
-    let content;
+  static handleData(res, data) {
+    let content = data;
     if (isArray(data)) {
       content = data.map(item => item.attributes);
-    } else {
+    } else if (data && data.attributes) {
       content = data.attributes;
     }
     res.json(content);
   }
 
-  handleError(res, err) {
+  static handleError(res, err) {
     res.json({ err: err });
   }
 
-  route(action, isInstanceAction, fn) {
-    const path = this.path(action, isInstanceAction);
-    const method = this.actionMethod(action);
-    this.router.route(path)[method](fn);
+  static payload(req) {
+    return assign({}, req.params, req.body, req.query);
   }
 
-  actionPath(action) {
-    const defaults = {
-      first: '/first',
-      last: '/last',
-      count: '/count',
-      insert: '/create',
-      delete: '/delete',
-    };
-    return this.Klass[action + 'ActionPath'] || defaults[action] || '';
-  }
+  static scopedKlass(Klass, req) {
+    const data = this.payload(req);
+    return class ScopedKlass extends Klass {
+      static get defaultScope() {
+        return JSON.parse(data.scope);
+      }
 
-  path(action, isInstanceAction) {
-    const name = pluralize(this.name, isInstanceAction ? 1 : 2);
-    const id = isInstanceAction ? ':id' : '';
-    const actionPath = trim(this.actionPath(action), '/');
-    const pathFragments = compact([name, id, actionPath]);
-    return pathFragments.join('/') + this.postfix;
-  }
+      static get defaultOrder() {
+        return JSON.parse(data.order);
+      }
 
-  scopedKlass(req) {
-    return class Klass extends this.Klass {
+      static get _skip() {
+        return JSON.parse(data.skip);
+      }
 
+      static get _limit() {
+        return JSON.parse(data.limit);
+      }
     };
   }
 
-  klass(req) {
-
+  static klass(Klass, req) {
+    const data = JSON.parse(this.payload(req).attributes);
+    const identifier = Klass.identifier;
+    if (data[identifier]) {
+      const query = { [identifier]: data[identifier] };
+      return Klass.model.where(query).first
+       .then(klass => klass.assign(omit(data, identifier)));
+    } else {
+      return Klass.model.promiseBuild(data);
+    }
   }
 
-  get path() {
-    return trim(this.Klass.routePath, '/');
+  constructor(app, router) {
+    this.app = app;
+    this.root = router.root;
+    this.routes = router.routes;
+    app.use(this.root, this.constructor.responseHeaders);
   }
 
-  get version() {
-    return trim(this.Klass.routeVersion, '/');
+  defaultActions(Klass) {
+    return {
+      all: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.scopedKlass(Klass, req).all
+        );
+      },
+      first: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.scopedKlass(Klass, req).first
+        );
+      },
+      last: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.scopedKlass(Klass, req).last
+        );
+      },
+      count: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.scopedKlass(Klass, req).count
+        );
+      },
+      create: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.klass(Klass, req)
+          .then(klass => klass.save())
+        );
+      },
+      show: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.klass(Klass, req)
+        );
+      },
+      update: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.klass(Klass, req)
+          .then(klass => klass.save())
+        );
+      },
+      delete: (req, res) => {
+        this.constructor.respond(res,
+          this.constructor.klass(Klass, req)
+          .then(klass => klass.delete())
+        );
+      },
+    };
   }
 
-  get name() {
-    return trim(this.Klass.routeName || this.Klass.tableName, '/');
+  controller(Klass, actionsParam) {
+    const router = express.Router();
+    const actions = defaults(actionsParam, this.defaultActions(Klass));
+    for (const route of this.routesForClass(Klass)) {
+      const action = actions[route.action];
+      if (action) {
+        router.route(route.url)[route.method](action);
+      } else {
+        console.log(`can't find action '${action}' for model '${Klass.modelName}'`);
+      }
+    }
+    this.app.use(this.root, router);
   }
 
-  get postfix() {
-    return get.Klass.routePostfix || '';
+  routesForClass(Klass) {
+    return filter(this.routes, (route) => (
+      route.modelName === Klass.modelName
+    ));
   }
 };
